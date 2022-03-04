7605315400e7439e60a1c230e340f45d2d1bd554
Carlos Eduardo Vieira <csilva.solks@digio.com.br>
2022-03-04 06:17:47 -0300

    initial commit

---
+++ bkp-tool.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+import argparse
+import subprocess
+import sys
+
+class style():
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+# Variáveis
+version = 0.1
+backup_dir = 'backup_dir.list'
+prog = 'bkp-tool'
+
+parser = argparse.ArgumentParser(description='bkp-tool - file backup tool using rsync', prog=f'{prog}')
+parser.add_argument('src', help='specify the source directory')
+parser.add_argument('dst', help='specify the destination directory')
+parser.add_argument('--file', '-f', default=f'{backup_dir}', help='specify the file with the directories to backup')
+parser.add_argument('--show-logs', action='store_true', help='shows the details of success as well as errors')
+parser.add_argument('--show-errors', action='store_true', help='show only error details')
+parser.add_argument('--version', '-v', action='version', version=f'%(prog)s {version}')
+args = parser.parse_args()
+
+print(f'{style.HEADER}starting {prog} {version} {style.ENDC}')
+
+err = {}
+logs = {}
+
+with open(args.file) as repo_backup:
+    directories = repo_backup.readlines()
+    if len(directories) == 0:
+        print(f'{style.WARNING}*** the list for backup (file: {args.file}) is empty{style.ENDC}')
+        sys.exit(0)
+    for dir in directories:
+        backup_run = subprocess.run(['rsync', '-avz', '--partial', '--ignore-errors', '--delete', f'{args.src}/{dir.strip()}', f'{args.dst}/{dir.strip()}'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if backup_run.returncode == 0:
+            logs.update({dir: 'success!'})
+        if backup_run.returncode != 0:
+            err.update({dir: backup_run.stderr})
+
+def show_logs():
+    if args.show_logs == True:
+        for folder in logs:
+            print(f'{style.OKGREEN}[successful] {folder.strip()} - {logs[folder].strip()}{style.ENDC}')
+
+def show_errors():
+    if args.show_logs or args.show_errors == True:
+        for err_dir in err:
+            print(f'{style.FAIL}[error] directory: {style.UNDERLINE}{err_dir.strip()}{style.ENDC}{style.FAIL} | details: {err[err_dir].strip()}{style.ENDC}')
+
+if len(err) == 0:
+    print(f'\n{style.OKGREEN}*** congratulations! all directories have been backed up successfully!{style.FAIL}\n')
+    show_logs()
+else:
+    print(f'\n{style.FAIL}*** problems detected when performing the backup!{style.ENDC}\n')
+    show_logs()
+    show_errors()
